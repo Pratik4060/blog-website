f65332cc2494091498a10da9091511d4a02ab0aa
pratik <raut70856@gmail.com>
2025-07-28 23:01:40 +0530

    logo is added

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
       <div className="w-full block">
         <Header />
         <main>
-          TODO: <Outlet />
+          <Outlet />
         </main>
         <Footer />
       </div>
--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Logo = ({ width = "100px" }) => {
-  return <div>Logo</div>;
+  return (
+    <img
+      src="/logo.png" // Make sure this path points to your actual logo image (e.g., in /public folder)
+      alt="App Logo"
+      style={{ width }}
+    />
+  );
 };
 
 export default Logo;
